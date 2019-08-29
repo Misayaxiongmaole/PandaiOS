08e7ce62903cb7d7dbcb5668dd4ad905c9d22227
熊猫 <mac@macs-imac.local>
2019-08-29 22:27:42 +0800

    项目初始化

---
+++ day by day/day by day/Controller/PDGuanZhuViewController.m
@@ -0,0 +1,32 @@
+//
+//  PDGuanZhuViewController.m
+//  day by day
+//
+//  Created by mac on 2019/8/29.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import "PDGuanZhuViewController.h"
+
+@interface PDGuanZhuViewController ()
+
+@end
+
+@implementation PDGuanZhuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ day by day/day by day/Controller/PDHomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  PDHomeViewController.h
+//  day by day
+//
+//  Created by mac on 2019/8/29.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
